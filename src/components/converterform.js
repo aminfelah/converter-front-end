bbafd1ac0836a3dc3f5db7140b38f67e29f3584a
amin felah <aminfelah@gmail.com>
2022-03-09 22:21:17 +0100

    first view

--- src/components/converterform.js
+++ src/components/converterform.js
@@ -2,10 +2,39 @@ import "./converterform.css";
 
 function Converterform() {
   return (
+    <div className="form-style">
     <div className="form-css">
-      <h1>form</h1>
+      <div className="form-container">
+        <div className="champ">
+          <label className="label-style">Amount</label>
+          <input className="input-style" />
+        </div>
+        <div className="champ">
+          <label className="label-style">from</label>
+          <select className="option-style" name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+        <img width="2%" height="10%" alt="hatch-title" src="/switcher.png" />
+        <div className="champ">
+          <label className="label-style">to</label>
+          <select className="option-style" name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+        <button class="dropbtn">convert</button>
       </div>
-
+      <h5 className="link-style">view conversion history</h5>
+    </div>
+    <h3 className="pre-result-style">0 USD=</h3>
+    <h1 className="result-style">0 EUR</h1>
+    </div>
   );
 }
 
